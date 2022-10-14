2daff6e6aaf0abbc052bc99a6f24d0a7f329fed3
Gabriele Fronzé <sucre.91@hotmail.it>
2022-10-14 23:04:43 +0200

    Update ram-power.sh

--- ram-power.sh
+++ ram-power.sh
@@ -6,13 +6,12 @@ RAM_POWER_DRAW["DDR2"]=4.5
 RAM_POWER_DRAW["DDR3"]=3
 RAM_POWER_DRAW["DDR4"]=3
 
-DMI_RAM=$(strace -f -e open dmidecode -t 17 2> /dev/null)
+CORRECTION=1.
+
+DMI_RAM=$(strace -f -e open dmidecode -t memory 2> /dev/null)
 N_STICKS=$(echo "$DMI_RAM" | grep DDR* | wc -l)
 RAM_TYPE=$(echo "$DMI_RAM" | grep -o DDR[1-4] | tail -1)
-
-echo $N_STICKS
-echo $RAM_TYPE
-echo ${RAM_POWER_DRAW[$RAM_TYPE]}
+IS_ECC=$(echo "$DMI_RAM" | grep 
 
 export ELEMENTO_POWER_RAM
 ELEMENTO_POWER_RAM=$(echo "$N_STICKS * ${RAM_POWER_DRAW[$RAM_TYPE]}" | bc)
