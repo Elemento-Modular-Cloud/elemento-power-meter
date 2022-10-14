4094a13941894ef8492582b3a1a903a5f8653c51
Gabriele Fronzé <sucre.91@hotmail.it>
2022-10-14 10:33:07 +0200

    Update cpu-power.sh

--- cpu-power.sh
+++ cpu-power.sh
@@ -1,7 +1,7 @@
 cmd="sensors -u | grep -A1 Esocket0: | tail -1 | grep -oP '(?<=\s)\d.+'"
-prevjoules=$(eval $cmd)
 while :
 do
+  prevjoules=$(eval $cmd)
   sleep .5s
   joules=$(eval $cmd)
   echo $(echo "($joules - $prevjoules) / .5" | bc)
