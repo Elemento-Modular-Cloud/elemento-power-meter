3202f12c6523d19e2f8c7a3364f551e660f21240
Gabriele Fronzé <sucre.91@hotmail.it>
2022-10-14 10:08:03 +0200

    Update cpu-power.sh

--- cpu-power.sh
+++ cpu-power.sh
@@ -4,6 +4,6 @@ while :
 do
   joules=$(sensors -u | grep -A1 Esocket0: | tail -1 | grep -oP '(?<=\s)\d.+')
   echo $joules
-  echo $($(echo "$d1 + $d2" | bc))
+  echo $($(echo "$joules - $prevjoules" | bc))
   prevjoules=$joules
 done
