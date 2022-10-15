7aa34476a06c7bc6749c37beabd697db004bdd27
Gabriele Fronzé <sucre.91@hotmail.it>
2022-10-15 21:48:15 +0200

    Create storage-power.sh

---
+++ storage-power.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NVME_POWER_DRAW=(25 18 18 15 10 8 5)
+
+export ELEMENTO_POWER_NVME
+NVME_DEVICES=$(nvme list | tail -n +3 | awk '{print $1}')
+
+while IFS= read -r dev; do
+    echo $(nvme get-feature "$dev" -f 2 | grep -o "0x0000000[0-9]" | tail -c 2)
+done <<< "$NVME_DEVICES"
