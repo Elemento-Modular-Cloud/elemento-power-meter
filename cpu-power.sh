12d6b9b092522eeb2411f83253d7daed9f3d2f12
Gabriele Fronzé <sucre.91@hotmail.it>
2022-10-15 21:58:37 +0200

    Update cpu-power.sh

--- cpu-power.sh
+++ cpu-power.sh
@@ -3,12 +3,9 @@ cmd="sensors -u | grep -A1 Esocket0: | tail -1 | grep -oP '(?<=\s)\d.+'"
 
 export ELEMENTO_POWER_CPU
 
-while :
-do
-  prevjoules=$(eval $cmd)
-  sleep .5s
-  joules=$(eval $cmd)
-  ELEMENTO_POWER_CPU=$(echo "($joules - $prevjoules) / .5" | bc)
-  echo $ELEMENTO_POWER_CPU
-  prevjoules=$joules
-done
+prevjoules=$(eval $cmd)
+sleep .5s
+joules=$(eval $cmd)
+ELEMENTO_POWER_CPU=$(echo "($joules - $prevjoules) / .5" | bc)
+
+echo $ELEMENTO_POWER_CPU
