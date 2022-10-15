7a67f0f4edac6395994f8adca6a7d4f3c8b932f8
Gabriele Fronzé <sucre.91@hotmail.it>
2022-10-15 21:49:53 +0200

    Update storage-power.sh

--- storage-power.sh
+++ storage-power.sh
@@ -5,6 +5,8 @@ NVME_POWER_DRAW=(25 18 18 15 10 8 5)
 export ELEMENTO_POWER_NVME
 NVME_DEVICES=$(nvme list | tail -n +3 | awk '{print $1}')
 
+echo $NVME_DEVICES
+
 while IFS= read -r dev; do
     echo $(nvme get-feature "$dev" -f 2 | grep -o "0x0000000[0-9]" | tail -c 2)
 done <<< "$NVME_DEVICES"
