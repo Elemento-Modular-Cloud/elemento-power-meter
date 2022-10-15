6d9b7e67691c486deebaaad52afa64c8f877ae05
Gabriele Fronzé <sucre.91@hotmail.it>
2022-10-15 21:59:03 +0200

    Update cpu-rapl-power.sh

--- cpu-rapl-power.sh
+++ cpu-rapl-power.sh
@@ -4,12 +4,9 @@ cmd="cat /sys/devices/virtual/powercap/intel-rapl/intel-rapl:0/energy_uj"
 
 export ELEMENTO_POWER_CPU
 
-while :
-do
-  prevjoules=$(eval $cmd)
-  sleep .5s
-  joules=$(eval $cmd)
-  ELEMENTO_POWER_CPU=$(echo "($joules - $prevjoules) / (.5 * 10^6)" | bc)
-  echo $ELEMENTO_POWER_CPU
-  prevjoules=$joules
-done
+prevjoules=$(eval $cmd)
+sleep .5s
+joules=$(eval $cmd)
+ELEMENTO_POWER_CPU=$(echo "($joules - $prevjoules) / (.5 * 10^6)" | bc)
+
+echo $ELEMENTO_POWER_CPU
