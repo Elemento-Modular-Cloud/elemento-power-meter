c8e93ea2e26e56d7865ff1a5eb2c8730cffd2808
root <root@ce-devkit01.elementohq>
2022-10-17 23:37:40 +0200

    Adding storage power to total power

--- total-power.sh
+++ total-power.sh
@@ -5,9 +5,11 @@ export ELEMENTO_POWER_TOTAL
 cpu=$(bash ./cpu-rapl-power.sh)
 ram=$(bash ./ram-power.sh)
 nvme=$(bash ./nvme-power.sh)
-ELEMENTO_POWER_TOTAL=$(echo "$cpu + $ram + $nvme" | bc)
+storage=$(bash ./storage-power.sh)
+ELEMENTO_POWER_TOTAL=$(echo "$cpu + $ram + $nvme + $storage" | bc)
 
 echo "CPU draw [W]: $cpu"
 echo "RAM draw [W]: $ram"
 echo "NVMe draw [W]: $nvme"
+echo "SATA/SAS draw [W]: $storage"
 echo "System draw [W]: $ELEMENTO_POWER_TOTAL"
