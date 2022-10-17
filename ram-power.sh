6f61c1bca4be8e66d1480ee5d71485ded8e8c069
root <root@ce-devkit01.elementohq>
2022-10-17 23:36:05 +0200

    Improve ram power

--- ram-power.sh
+++ ram-power.sh
@@ -11,9 +11,9 @@ CORRECTION=1.
 DMI_RAM=$(strace -f -e open dmidecode -t 17 2> /dev/null)
 N_STICKS=$(echo "$DMI_RAM" | grep DDR* | wc -l)
 RAM_TYPE=$(echo "$DMI_RAM" | grep -o DDR[1-4] | tail -1)
-IS_ECC=$(dmidecode -t memory | grep -q "Error Correction Type: None")
+IS_ECC=$(dmidecode -t memory | grep -c "Error Correction Type: None")
 
-if $IS_ECC; then
+if [[ $IS_ECC ]]; then
   CORRECTION=1.125
 fi
 
