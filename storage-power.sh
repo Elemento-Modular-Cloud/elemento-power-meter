9a296f0d301874d3de2be5d57a3c403c880d9005
Gabriele Fronzé <sucre.91@hotmail.it>
2022-10-18 10:42:13 +0200

    Fixing storage power

--- storage-power.sh
+++ storage-power.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 declare -a STORAGE_POWER_DRAW
-STORAGE_POWER_DRAW["Solid-State-Device"]=4.2
+STORAGE_POWER_DRAW["SolidStateDevice"]=4.2
 STORAGE_POWER_DRAW["15000"]=6.5
 STORAGE_POWER_DRAW["10000"]=5.8
 STORAGE_POWER_DRAW["7200"]=8
@@ -11,7 +11,7 @@ STORAGE_DEVICES=$(lsscsi -t | grep -i "sata" | awk '{print $NF}')
 
 while IFS= read -r dev; do
     if [[ ! -z "$dev" ]]; then
-        type=$(hdparm -I $dev | grep -e "Nominal Media Rotation Rate:" | cut -d":" -f2 | tr ' ' '-')
+        type=$(hdparm -I $dev | grep -e "Nominal Media Rotation Rate:" | cut -d":" -f2 | tr -d ' ')
         ELEMENTO_POWER_STORAGE=$(echo "$ELEMENTO_POWER_STORAGE + ${STORAGE_POWER_DRAW[$type]}" | bc)
     fi
 done <<< "$STORAGE_DEVICES"
