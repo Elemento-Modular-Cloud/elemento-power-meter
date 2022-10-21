a9eafe5b60518b355f7045c4a9489f7039acb330
Gabriele Fronzé <sucre.91@hotmail.it>
2022-10-21 19:14:59 +0200

    Getting nvme power states from smartctl

--- nvme-power.sh
+++ nvme-power.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-NVME_POWER_DRAW=(25 18 18 15 10 8 5)
-
 export ELEMENTO_POWER_NVME=0
 NVME_DEVICES=$(nvme list | tail -n +3 | awk '{print $1}')
 
 while IFS= read -r dev; do
     if [[ ! -z "$dev" ]]
     then
+        power_states=( $(smartctl -a "$dev" | sed -n '/Supported Power States/,/^ *$/p' | head -n -1 | tail -n +3 | grep -Eo '[+-]?[0-9]+([.][0-9]+)?W') )
         power_state=$(nvme get-feature "$dev" -f 2 | grep -o "0x0000000[0-9]" | tail -c 2)
-        ELEMENTO_POWER_NVME=$(echo "$ELEMENTO_POWER_NVME + ${NVME_POWER_DRAW[$power_state]}" | bc)
+        ELEMENTO_POWER_NVME=$(echo "$ELEMENTO_POWER_NVME + ${power_states[$power_state - 1]}" | bc)
     fi
 done <<< "$NVME_DEVICES"
 
