46f3546fe0ad66188119cd8abf493544e751f28e
Gabriele Fronzé <sucre.91@hotmail.it>
2022-10-21 19:27:00 +0200

    Introducing PSU efficiency factor

--- total-power.sh
+++ total-power.sh
@@ -7,12 +7,16 @@ ram=$(bash ./ram-power.sh)
 nvme=$(bash ./nvme-power.sh)
 storage=$(bash ./storage-power.sh)
 ELEMENTO_POWER_TOTAL=$(echo "$cpu + $ram + $nvme + $storage" | bc)
+efficiency=$(bash ./psu-efficiency.sh)
+ELEMENTO_POWER_WALL=$(echo "$ELEMENTO_POWER_TOTAL" / "$efficiency" | bc)
 
 echo "--------------------------------------"
 printf "CPU draw [W]: %.1f\n" $cpu
 printf "RAM draw [W]: %.1f\n" $ram
 printf "NVMe draw [W]: %.1f\n" $nvme
 printf "SATA/SAS draw [W]: %.1f\n" $storage
+printf "PSU efficiency [%]: %.1f\n" $storage
 echo "--------------------------------------"
-printf "System draw [W]:  %.1f\n" $ELEMENTO_POWER_TOTAL
+printf "System consumption [W]:  %.1f\n" $ELEMENTO_POWER_TOTAL
+printf "System wall draw [W]:  %.1f\n" $ELEMENTO_POWER_WALL
 echo "--------------------------------------"
