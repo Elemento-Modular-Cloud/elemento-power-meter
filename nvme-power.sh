e8f52a5d8c911a65e748fd572624246740727578
Gabriele Fronzé <sucre.91@hotmail.it>
2022-10-21 19:20:48 +0200

    Getting power state in more readable form

--- nvme-power.sh
+++ nvme-power.sh
@@ -7,8 +7,8 @@ while IFS= read -r dev; do
     if [[ ! -z "$dev" ]]
     then
         power_states=( $(smartctl -a "$dev" | sed -n '/Supported Power States/,/^ *$/p' | head -n -1 | tail -n +3 | grep -Eo '[+-]?[0-9]+([.][0-9]+)?W') )
-        power_state=$(nvme get-feature "$dev" -f 2 | grep -o "0x0000000[0-9]" | tail -c 2)
-        ELEMENTO_POWER_NVME=$(echo "$ELEMENTO_POWER_NVME + ${power_states[$power_state - 1]}" | bc)
+        power_state=$(nvme get-feature "$dev" -f 2 -H | tail -1 | grep -o [0-9])
+        ELEMENTO_POWER_NVME=$(echo "$ELEMENTO_POWER_NVME + ${power_states[$power_state]}" | bc)
     fi
 done <<< "$NVME_DEVICES"
 
