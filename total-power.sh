90ea770b9c0e07ba305bf02e1eec2e337b9373cc
Gabriele Fronzé <sucre.91@hotmail.it>
2022-10-24 00:23:22 +0200

    PSU efficiency precision

--- total-power.sh
+++ total-power.sh
@@ -18,7 +18,7 @@ printf "CPU draw [W]: %.1f\n" $cpu
 printf "RAM draw [W]: %.1f\n" $ram
 printf "NVMe draw [W]: %.1f\n" $nvme
 printf "SATA/SAS draw [W]: %.1f\n" $storage
-printf "PSU efficiency [%%]: %.1f\n" $efficiency
+printf "PSU efficiency [%%]: %.3f\n" $efficiency
 echo "--------------------------------------"
 printf "System consumption [W]:  %.1f\n" $ELEMENTO_POWER_TOTAL
 printf "System wall draw [W]:  %.1f\n" $ELEMENTO_POWER_WALL
