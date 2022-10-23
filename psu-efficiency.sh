7d00fc29512e2f7fc2981e73b4b2ea2f0e6b7ff3
Gabriele Fronzé <sucre.91@hotmail.it>
2022-10-24 00:19:52 +0200

    Final fixes

--- psu-efficiency.sh
+++ psu-efficiency.sh
@@ -14,7 +14,8 @@ EFFCURVES[Gold]="-0.0376, 2.73, 47.429"
 EFFCURVES[Platinum]="-0.0389, 2.79, 49.762"
 EFFCURVES[Titanium]="-0.0405, 2.90, 52.190"
 
-readarray -td, P <<<"${EFFCURVES[$EFFICIENCY]}"; declare -p P;
+readarray -td, P <<<"${EFFCURVES[$EFFICIENCY]}"
 
-ELEMENTO_PSU_EFFICIENCY=$(echo "0.01 * (${P[0]} * $LOAD_PC * $LOAD_PC + ${P[1]} * $LOAD_PC + ${P[2]})" | bc)
+ELEMENTO_PSU_EFFICIENCY=`bc -l <<< "${P[0]} * $LOAD_PC * $LOAD_PC + ${P[1]} * $LOAD_PC + ${P[2]}"`
+ELEMENTO_PSU_EFFICIENCY=`bc -l <<< "scale=4; $ELEMENTO_PSU_EFFICIENCY / 100"`
 echo $ELEMENTO_PSU_EFFICIENCY
