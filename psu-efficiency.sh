262851d04b2d46d1fec7d33e23ef7c5edafc0e96
Gabriele Fronzé <sucre.91@hotmail.it>
2022-10-24 00:03:08 +0200

    Adding % scaling

--- psu-efficiency.sh
+++ psu-efficiency.sh
@@ -2,7 +2,8 @@
 
 LOAD=$1
 MAX_LOAD=${2:-"1600"}
-LOAD_PC=${bc -t <<< $LOAD / $MAX_LOAD}
+
+LOAD_PC=`bc -l <<< "$LOAD / $MAX_LOAD * 100"`
 EFFICIENCY=${3:-"Gold"}
 
 declare -A EFFCURVES
@@ -13,13 +14,7 @@ EFFCURVES[Gold]="-0.0376, 2.73, 47.429"
 EFFCURVES[Platinum]="-0.0389, 2.79, 49.762"
 EFFCURVES[Titanium]="-0.0405, 2.90, 52.190"
 
-echo $EFFICIENCY
-echo ${EFFCURVES[$EFFICIENCY]}
-
 readarray -td, P <<<"${EFFCURVES[$EFFICIENCY]}"; declare -p P;
 
-echo $P
-
-ELEMENTO_PSU_EFFICIENCY=$(echo ${P[0]} * $LOAD_PC * $LOAD_PC + ${P[1]} * $LOAD_PC + ${P[2]}  | bc)
-
+ELEMENTO_PSU_EFFICIENCY=$(echo "(${P[0]} * $LOAD_PC * $LOAD_PC + ${P[1]} * $LOAD_PC + ${P[2]}) / 100" | bc)
 echo $ELEMENTO_PSU_EFFICIENCY
