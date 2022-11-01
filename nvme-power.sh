59860e814c2089112c4912bfabacfd37f4a9408e
Gabriele Fronzé <sucre.91@hotmail.it>
2022-11-02 00:52:55 +0100

    Add modifier to nvme drives

--- nvme-power.sh
+++ nvme-power.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+isActive() {
+    device=$(echo "$1" | cut -d "/" -f3)
+    cmd='cat /proc/diskstats | grep "$device"'
+    stats0=$(eval $cmd)
+    sleep .1s
+    stats1=$(eval $cmd)
+    echo $(diff  <(echo "$stats0" ) <(echo "$stats1"))
+    return 0
+}
+
+activityModifier() {
+    active=$(isActive $1)
+    if [ "$active" != "" ]; then
+        echo 1.
+        return 0
+    fi
+    echo 0.2
+    return 0
+}
+
 export ELEMENTO_POWER_NVME=0
 NVME_DEVICES=$(nvme list | tail -n +3 | awk '{print $1}')
 
@@ -8,7 +28,24 @@ while IFS= read -r dev; do
     then
         power_states=( $(smartctl -a "$dev" | sed -n '/Supported Power States/,/^ *$/p' | head -n -1 | tail -n +3 | grep -Eo '[+-]?[0-9]+([.][0-9]+)?W') )
         power_state=$(nvme get-feature "$dev" -f 2 -H | tail -1 | grep -o [0-9])
-        ELEMENTO_POWER_NVME=$(echo "$ELEMENTO_POWER_NVME + ${power_states[$power_state]}" | bc)
+        state=$(hdparm -C $dev | grep -e "drive state is:" | cut -d ":" -f2 | tr -d ' ')
+        modifier=1.
+        case $state in
+            "unknown")
+                modifier=$(activityModifier $dev)
+                ;;
+            "active/idle")
+                modifier=$(activityModifier $dev)
+                ;;
+            "standby")
+                modifier=.2
+                ;;
+            "sleeping")
+                modifier=.1
+                ;;
+        esac
+        devicepower=$(echo "${power_states[$power_state]} * $modifier" | bc)
+        ELEMENTO_POWER_NVME=$(echo "$ELEMENTO_POWER_NVME + $devicepower" | bc)
     fi
 done <<< "$NVME_DEVICES"
 
